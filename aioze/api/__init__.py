0476626153b0bfc733aff48cf8b32ab48354f791
lesensei <alain.perry@gmail.com>
2022-04-20 19:53:05 +0200

    Added user info API

--- aioze/api/__init__.py
+++ aioze/api/__init__.py
@@ -7,3 +7,4 @@ from aioze.api.punishment import Punishment
 from aioze.api.notification import Notification
 from aioze.api.information import Information
 from aioze.api.homework import Homework
+from aioze.api.userinfo import UserInfo
---
+++ aioze/api/userinfo.py
@@ -0,0 +1,25 @@
+import json
+import logging
+from datetime import datetime, timedelta
+from urllib.parse import urljoin
+
+from aioze.access import Access
+
+_LOGGER = logging.getLogger(__package__)
+
+
+class UserInfo:
+    def __init__(self, access: Access):
+        self._access = access
+
+    async def get_user_info(self):
+        """Get authenticated user information"""
+        if not await self._access.is_authenticated():
+            await self._access.authenticate()
+        user_info_url = urljoin(self._access.api_url, "v1/users/me")
+        _LOGGER.debug("Getting '%s'", user_info_url)
+        res = await self._access.api_wrapper("get", user_info_url)
+        if res.status != 200:
+            _LOGGER.error("Error fetching Oze user information: '%s'", res.reason)
+            return
+        return await res.json()
